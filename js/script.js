61edc622ca5c017e705dbd726a8a65224593516d
Elignobis <rwayisibongile@gmail.com>
2022-05-10 11:50:16 +0200

    footer mistake

--- js/script.js
+++ js/script.js
@@ -42,3 +42,14 @@ $(document).ready(function() {
       });
     });
   });
+
+
+
+
+  function readMore() {
+    var expandInfo = document.getElementById("more-info-js");
+    var mainHeadings = document.getElementById("main-headings-js");
+    
+    mainHeadings.style.transform = "scale(0.7)";
+    expandInfo.style.height = "350px";
+  } 
