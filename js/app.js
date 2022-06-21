0844af16fd152d81d25c15b4bbb13334c36322b9
Elignobis <rwayisibongile@gmail.com>
2022-06-21 15:32:13 +0200

    blurry index added

--- js/app.js
+++ js/app.js
@@ -102,3 +102,36 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // ADD A load EVENT LISTENER TO THE window, THEN RUN THE fadeEffect FUNCTION
 window.addEventListener('load', fadeEffect);
+
+
+
+// ADDING ARRAYS 
+
+const testimonials =[ {
+	fullName: "Joel Mukanya",
+	imgURL: "link",
+	testimonial:"Sibongile is a well-behaved, outstanding, self-motivated and helpful person. Sibongile works well with team and do well working alone, he always submit his work on time which is very functional. I will suggest you to hire him on the spot"
+	},
+	{
+	fullName: "Hannah Dalwai",
+	imgURL: "link",
+	testimonial:"Sibongile is a diligent and driven individual with a keen eye for design. He is meticulous and always meets his deadlines in a timely manner. He will be a valuable asset to any team lucky enough to have him."
+	},
+	{
+	fullName: "Amanda Gudlwa",
+	imgURL: "link",
+	testimonial:"Sibongile is a really great developer, always learning and in the forefront of new ideas and innovations. And always avails himself to help others when the need arises."
+	},
+	{
+	fullName: "Philani Mxathule",
+	imgURL: "link",
+	testimonial:"Sbo is a very hardworking and intellegent person, who will do anything in his power to help a person in need. Having him in any team is an advantage"
+	},
+	{
+	fullName: "Siliziwe Pani",
+	imgURL: "link",
+	testimonial:"Sibongile is a great web developer, and has a a bright future. He is innovative and always ready to learn and educate others."
+	},
+];
+
+console.table(testimonials);
